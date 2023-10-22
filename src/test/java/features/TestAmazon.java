7f9c5380e515dd7070473eec2585bb1da3b07960
CORP\cobcouli <cheikhomarben.coulibaly@sogeti.com>
2023-10-22 22:06:44 +0200

    update day 2

--- src/test/java/features/TestAmazon.java
+++ src/test/java/features/TestAmazon.java
@@ -4,10 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chromium.ChromiumDriver;
-
-import org.openqa.selenium.remote.Browser;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
